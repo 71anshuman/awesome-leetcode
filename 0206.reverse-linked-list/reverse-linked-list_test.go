336ead2391ff5c1635c91859b1a221f51e680df1
Anshuman lawania <71anshuman@gmail.com>
2021-08-09 00:52:11 +0530

    added-0206

---
+++ 0206.reverse-linked-list/reverse-linked-list_test.go
@@ -0,0 +1,72 @@
+package problem0206
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+type para struct {
+	one []int
+}
+
+type ans struct {
+	one []int
+}
+
+func Test_Problem0206(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		{
+			para{[]int{1, 2, 3, 4, 5}},
+			ans{[]int{5, 4, 3, 2, 1}},
+		},
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, l2s(reverseList(s2l(p.one))), "%v", p)
+	}
+}
+
+// convert *ListNode to []int
+func l2s(head *ListNode) []int {
+	res := []int{}
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+// convert []int to *ListNode
+func s2l(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := &ListNode{
+		Val: nums[0],
+	}
+	temp := res
+	for i := 1; i < len(nums); i++ {
+		temp.Next = &ListNode{
+			Val: nums[i],
+		}
+		temp = temp.Next
+	}
+
+	return res
+}
